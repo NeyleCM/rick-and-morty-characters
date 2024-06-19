d9b8361c437b32d5db7cf2f1c3a73d981b64772d
NeyleCM <neylecmorantes@gmail.com>
2024-06-20 00:27:15 +0200

    Listo

--- js/script.js
+++ js/script.js
@@ -1,30 +1,48 @@
 
+//variables
 
-function getPersonajes(datos) {
-const resultados = fetch('https://rickandmortyapi.com/api/character');
+let numero = 1;
+const btnPrev = document.getElementById('prev-page');
+const btnNext = document.getElementById('next-page');
+const vistaPersonajes = document.getElementById('character-list')
+
+//FUNCIÓN BOTONES
+
+btnNext.addEventListener('click', () =>{
+ if(numero <= 42) {
+    numero += 1;
+ getPersonajes(listaPersonajes);
+}
+})
+
+btnPrev.addEventListener('click', () =>{
+    if (numero > 1);
+    numero -= 1;
+    getPersonajes(listaPersonajes);
+   })
+
+   // FETCH
+
+function getPersonajes(callback) {
+const resultados = fetch(`https://rickandmortyapi.com/api/character/?page=${numero}`);
 resultados
-.then(response =>
-    response.json())
-    .then(data => {
-datos(data)
-    });
+.then(response => response.json())
+    .then(data => callback(data))
+        .catch(error => console.error('Error en los datos', error))
 }
 
 
-getPersonajes(data =>
-data.results.forEach(personaje => {
-    const listaUl = document.getElementById('character-list');
-    listaUl.innerHTML += `<li class='personajesTarjeta'>
+const listaPersonajes = (data) => {
+vistaPersonajes.innerHTML = '';
+    data.results.forEach(personaje => {
+ const contenedor = document.getElementById('character-list').innerHTML += 
+ `<li class='personajesTarjeta'>
 <img src=${personaje.image} 
 alt=${personaje.name}/>
 <h2>${personaje.name}</h2>
-<p>${personaje.species}</p></li>`
-
-
-getPersonajes(personaje);
+<p>${personaje.species}</p></li>`;
 
 })
-)
+}
 
-
-
+getPersonajes(listaPersonajes);
